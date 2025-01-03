460e91789f9a30d3f2b8377d87a27fad565de158
BugHunter-X <askzenai@gmail.com>
2025-01-03 15:18:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,8 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  result = input * 2; 
+end
+
+% Call the function after it is defined
+input = [1, 2, 3, 4, 5];
+result = myFunction(input); 
